7833429767beecce6e5b832ad25f4e37c1a536f9
Novello <dnovello@minsait.com>
2023-05-24 15:45:23 +0200

    Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers', // Selector by Tag
-  //selector: '[app-servers]', //Selector by property -> attribute into tags eg.
-  //selector: '.app-servers', // Selector by Class... n.b. isn't supported by ID or others )
-  // template: `<h1>Titolo di prova</h1><app-server></app-server><app-server></app-server>`,
+  selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
   }
   ngOnInit() {}
+  //Naming convention: "on" before "CreateServer" mean an action (event) to do something
+  onCreateServer() {
+    this.serverCreationStatus = 'Server created!!';
+  }
 }
