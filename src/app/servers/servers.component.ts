7e695da8a26e392eda8698b796bd5e35fb0ecf85
Novello <dnovello@minsait.com>
2023-05-24 17:49:26 +0200

    Passing and Using Data with Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
@@ -16,5 +17,9 @@ export class ServersComponent implements OnInit {
   //Naming convention: "on" before "CreateServer" mean an action (event) to do something
   onCreateServer() {
     this.serverCreationStatus = 'Server created!!';
+  }
+  onUpdateServerName(event: Event) {
+    console.log(event); //Eg. se scrivo "test" nel campo input, nella console cercando all'interno di uno qualsiasi degli eventi e sotto la voce InputEvent{}/target:/ troverò il valore inserito value:"test". Grazie a questo dato possiamo impostare unanuova properyy serverName = '' (sopra) quindi preleviamo il dato dal dOM con questa sintassi: (<HTMLInputElement>event.target) ed aggiungendo il dato "value" dall'oggetto InputEvent
+    this.serverName = (<HTMLInputElement>event.target).value; //si usa "event" in quanto è la variabile riservata ($event) presente nel DOM
   }
 }
