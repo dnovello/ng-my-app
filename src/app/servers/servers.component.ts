f1b68462cfa6c7bcf4fbbe7667b9a5e8c10a6144
Novello <dnovello@minsait.com>
2023-05-25 17:39:52 +0200

    38. Enhancing ngIf with an Else Condition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,6 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
-  //37. Directives: Using ngIf to Output Data Conditionally
   serverCreated = false;
 
   constructor() {
@@ -18,7 +17,6 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
-    //37. Directives: Using ngIf to Output Data Conditionally
     this.serverCreated = true;
     this.serverCreationStatus =
       'Server created! The name is: ' + this.serverName;
