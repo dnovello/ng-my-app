81ef631b90cf4468b54e62a7351ec8d3977faaa0
Novello <dnovello@minsait.com>
2023-05-25 23:50:13 +0200

    40. Applying CSS Classes Dynamically with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,6 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  // 40. Applying CSS Classes Dynamically with ngClass
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverId: number = 10;
@@ -10,13 +18,13 @@ export class ServerComponent {
   //39. Styling Elements Dynamically with ngStyle: Aggiunto constructor
   constructor() {
     //39. Styling Elements Dynamically with ngStyle: valore random per simulare online ed offline del server
-    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
 
   getServerStatus() {
     return this.serverStatus;
   }
   getColor() {
-    return this.serverStatus === 'Online' ? 'green' : 'red';
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
