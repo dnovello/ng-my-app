3c0faf9c3f0303da17d6510d48d080f20c4e2c21
Novello <dnovello@minsait.com>
2023-05-26 17:23:58 +0200

    41. Outputting Lists with ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = '';
   serverCreated = false;
+  //41. Outputting Lists with ngFor 
+  servers = [];
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
@@ -20,6 +22,8 @@ export class ServersComponent implements OnInit {
     this.serverCreated = true;
     this.serverCreationStatus =
       'Server created! The name is: ' + this.serverName;
+    //41. Outputting Lists with ngFor
+    this.servers.push(this.serverName);
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
