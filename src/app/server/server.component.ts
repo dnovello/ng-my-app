9329915f2f8eef0ad0de157e3364ecd1602871b0
Novello <dnovello@minsait.com>
2023-06-05 17:33:16 +0200

    42. [OPTIONAL] Assignment Solution

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,29 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  // 40. Applying CSS Classes Dynamically with ngClass
-  styles: [
-    `
-      .online {
-        color: white;
-      }
-    `,
-  ],
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
-  serverId: number = 10;
-  serverStatus: string = 'offline';
-  //39. Styling Elements Dynamically with ngStyle: Aggiunto constructor
-
-  constructor() {
-    //39. Styling Elements Dynamically with ngStyle: valore random per simulare online ed offline del server
-    // this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
-  }
-
-  getServerStatus() {
-    return this.serverStatus;
-  }
-  getColor() {
-    return this.serverStatus === 'online' ? 'green' : 'red';
-  }
+  constructor() {}
 }
